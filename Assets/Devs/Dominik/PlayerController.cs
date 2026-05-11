8ed41f3c5d933f0efc0004735425ff48569fef09
xWaves773 <dominik.drenth@gmail.com>
2026-05-11 12:08:45 +0200

    feat: Input Actions Player

---
+++ Assets/Devs/Dominik/PlayerController.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float playerSpeed = 5.0f;
+    [SerializeField]
+    private float jumpHeight = 1.5f;
+    [SerializeField]
+    private float gravityValue = -9.81f;
+
+    public CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+
+    [Header("Input Actions")]
+    public InputActionReference moveAction;
+    public InputActionReference jumpAction;
+
+    private InputManager inputManager;
+
+    private void OnEnable()
+    {
+        moveAction.action.Enable();
+        jumpAction.action.Enable();
+        inputManager = InputManager.Instance;
+    }
+
+    private void OnDisable()
+    {
+        moveAction.action.Disable();
+        jumpAction.action.Disable();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+
+        if (groundedPlayer)
+        {
+            // Slight downward velocity to keep grounded stable
+            if (playerVelocity.y < -2f)
+                playerVelocity.y = -2f;
+        }
+
+        // Read input
+        Vector2 movement = inputManager.GetPlayerMovement();
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        if (move != Vector3.zero)
+            transform.forward = move;
+
+        // Jump using WasPressedThisFrame()
+        if (groundedPlayer && inputManager.PlayerJumpedThisFrame())
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
+        }
+
+        // Apply gravity
+        playerVelocity.y += gravityValue * Time.deltaTime;
+
+        // Move
+        Vector3 finalMove = move * playerSpeed + Vector3.up * playerVelocity.y;
+        controller.Move(finalMove * Time.deltaTime);
+    }
+}
