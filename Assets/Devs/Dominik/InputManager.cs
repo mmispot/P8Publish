eeb45bafe30c3bd38576c7397e6c0aa32e507af7
xWaves773 <dominik.drenth@gmail.com>
2026-05-18 10:34:59 +0200

    feat: fps camera with headbobbing

--- Assets/Devs/Dominik/InputManager.cs
+++ Assets/Devs/Dominik/InputManager.cs
@@ -26,6 +26,7 @@ public class InputManager : MonoBehaviour
         }
             
         playerControls = new PlayerControls();
+        Cursor.visible = false;
     }
 
     private void OnEnable()
