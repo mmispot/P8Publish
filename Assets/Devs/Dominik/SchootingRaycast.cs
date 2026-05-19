6c9c0190219ca04013a0d511fca9d0a20c8ca533
xWaves773 <dominik.drenth@gmail.com>
2026-05-19 14:12:26 +0200

    feat: raycast shooting script

---
+++ Assets/Devs/Dominik/SchootingRaycast.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SchootingRaycast : MonoBehaviour
+{
+    public Transform FirePoint;
+
+    void Update()
+    {
+        
+    }
+
+    public void Shooting()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(FirePoint.position , transform.TransformDirection(Vector3.forward) , out hit , 100))
+        {
+            Debug.DrawRay(FirePoint.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+        }
+    }
+}
