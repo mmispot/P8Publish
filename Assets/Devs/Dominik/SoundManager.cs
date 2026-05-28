7a9caeab7b68325c99c62c1241ab1b187845cf99
xWaves773 <dominik.drenth@gmail.com>
2026-05-28 10:58:53 +0200

    fix: Sound Manager af

--- Assets/Devs/Dominik/SoundManager.cs
+++ Assets/Devs/Dominik/SoundManager.cs
@@ -1,35 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-//als je een sound ergens moet aaroepen bijvoorbeeld bij een player actie zet dit eronder: SoundManager.PlaySound(SoundType."de enum die je nodig hebt uit de lijst");
 
 public enum SoundType
 {
     TEST,
-    //JUMP,
-    //WALK,
-    //DEATH,
-    //PEEPER,
-    //SQUIDDLE,
-    //SPINO,
-    //MAIN_THEME,
-    //DEATH_THEME,
+    JUMP,
+    WALK,
+    DEATH,
+    PEEPER,
+}
 
-
+[System.Serializable]
+public class SoundEntry
+{
+    public SoundType soundType;
+    public AudioClip[] clips; //kan meerdere soundeffects hebben
 }
 
 [RequireComponent(typeof(AudioSource))]
-
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField] private AudioClip[] soundList;
+    [SerializeField] private SoundEntry[] soundList;
+
     private static SoundManager instance;
     private AudioSource audioSource;
+    private Dictionary<SoundType, AudioClip[]> soundDictionary;
 
     private void Awake()
     {
         instance = this;
+
+        soundDictionary = new Dictionary<SoundType, AudioClip[]>();
+
+        foreach (var entry in soundList)
+        {
+            soundDictionary[entry.soundType] = entry.clips;
+        }
     }
 
     private void Start()
@@ -39,7 +45,14 @@ public class SoundManager : MonoBehaviour
 
     public static void PlaySound(SoundType sound, float volume = 1)
     {
-        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+        if (instance.soundDictionary.TryGetValue(sound, out AudioClip[] clips))
+        {
+            AudioClip randomClip = clips[Random.Range(0, clips.Length)];
+            instance.audioSource.PlayOneShot(randomClip, volume);
+        }
+        else
+        {
+            Debug.LogWarning($"Sound niet gevonden: {sound}");
+        }
     }
-
 }
