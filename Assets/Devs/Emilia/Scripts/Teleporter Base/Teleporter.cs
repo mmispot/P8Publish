abfc3af658158367eadce559598aec63a121c2e0
Emilia Beeke <emiliadeboer1@gmail.com>
2026-06-11 11:25:16 +0200

    bug: teleporter doesn't work: player stuck to navmesh?

--- Assets/Devs/Emilia/Scripts/Teleporter Base/Teleporter.cs
+++ Assets/Devs/Emilia/Scripts/Teleporter Base/Teleporter.cs
@@ -1,32 +1,34 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class Teleporter : MonoBehaviour
 {
-    public Transform teleporterBase;
-    public Transform teleporterMap;
-
+    public Transform spawnPoint;   // empty GameObject placed where player should land
+    public GameObject destination; // the OTHER teleporter (to disable on arrival)
     public GameObject player;
 
     public float cooldown = 2f;
     private static float lastTeleportTime = -Mathf.Infinity;
 
-
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player && Time.time > lastTeleportTime + cooldown)
         {
+            if (spawnPoint == null) { Debug.LogError("spawnPoint not assigned on " + gameObject.name); return; }
+            if (destination == null) { Debug.LogError("destination not assigned on " + gameObject.name); return; }
+
+            Debug.Log("Teleporter hit: " + gameObject.name + " → sending to " + spawnPoint.position);
             lastTeleportTime = Time.time;
 
-            if (gameObject == teleporterBase)
-            {
-                Debug.Log("base teleporter hit");
-                player.transform.position = teleporterMap.transform.position;
-            }
-            else if (gameObject == teleporterMap)
-            {
-                Debug.Log("map teleporter hit");
-                player.transform.position = teleporterBase.transform.position;
-            }
+            player.transform.position = spawnPoint.position;
+            //StartCoroutine(DisableTemporarily(destination));
         }
     }
+
+    //private IEnumerator DisableTemporarily(GameObject teleporter)
+    //{
+    //    teleporter.GetComponent<Collider>().enabled = false;
+    //    yield return new WaitForSeconds(cooldown);
+    //    teleporter.GetComponent<Collider>().enabled = true;
+    //}
 }
