570bf7fc9206bf6557664222a20376531eacb02a
xWaves773 <dominik.drenth@gmail.com>
2026-06-15 09:33:13 +0200

    Feat: Radiation zone works

---
+++ Assets/Devs/Dominik/RadiationEffect.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class RadiationEffect : MonoBehaviour
+{
+    [SerializeField] private Material radiationMaterial;
+
+    private RadiationManager radiationManager;
+    private static readonly int IntensityIonisation = Shader.PropertyToID("_IntensityIonisation");
+
+    private void Awake()
+    {
+        radiationManager = FindObjectOfType<RadiationManager>();
+    }
+
+    private void Update()
+    {
+        if (radiationMaterial == null || radiationManager == null) return;
+
+        radiationMaterial.SetFloat(IntensityIonisation, radiationManager.CurrentRadiation);
+    }
+}
