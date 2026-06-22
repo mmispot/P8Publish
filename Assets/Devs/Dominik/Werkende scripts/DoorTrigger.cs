aa7b43d01bbbdd4674d6cc5266b95cc88b4f5ffa
xWaves773 <dominik.drenth@gmail.com>
2026-06-22 11:13:10 +0200

    fix: doors should stay open when the player stand still in the doorframe

--- Assets/Devs/Dominik/Werkende scripts/DoorTrigger.cs
+++ Assets/Devs/Dominik/Werkende scripts/DoorTrigger.cs
@@ -4,15 +4,21 @@ public class DoorTrigger : MonoBehaviour
 {
     public Door door;
 
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
             door.Open(other.transform);
+        }
     }
+
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
-            door.Close();
+        {
+            door.StartClose();
+        }
     }
 }
