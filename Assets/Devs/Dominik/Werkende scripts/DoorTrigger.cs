8e0dfb33c51a04a23eec45ac4088e1e7ee9920f8
Emilia Beeke <emiliadeboer1@gmail.com>
2026-06-23 12:06:10 +0200

    fix: player scripts

--- Assets/Devs/Dominik/Werkende scripts/DoorTrigger.cs
+++ Assets/Devs/Dominik/Werkende scripts/DoorTrigger.cs
@@ -2,23 +2,39 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
-    public Door door;
+    public Door doorScript;
 
+    private void Update()
+    {
+        if (doorScript == null)
+        {
+            Debug.LogError("Door script is not assigned in the inspector.");
+            doorScript = GetComponentInParent<Door>();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            door.Open(other.transform);
-        }
+        //if (other.CompareTag("Player"))
+        //{
+        //    Debug.Log("Player entered the trigger");
+        //    doorScript.Open(other.transform);
+        //}
+
+        Debug.Log("Player entered the trigger");
+        doorScript.Open(other.transform);
     }
 
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            door.StartClose();
-        }
+        //if (other.CompareTag("Player"))
+        //{
+        //    Debug.Log("Player exited the trigger");
+        //    doorScript.StartClose();
+        //}
+
+        Debug.Log("Player exited the trigger");
+        doorScript.StartClose();
     }
 }
