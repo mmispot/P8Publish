55b698912e940d532b3bd1e6bf818aea86d7d4c5
Emilia Beeke <emiliadeboer1@gmail.com>
2026-06-29 11:43:49 +0200

    fix: sounds fixed!

--- Assets/Devs/Dominik/Werkende scripts/SoundManager.cs
+++ Assets/Devs/Dominik/Werkende scripts/SoundManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public enum SoundType
 {
@@ -15,7 +16,7 @@ public enum SoundType
 public class SoundEntry
 {
     public SoundType soundType;
-    public AudioClip[] clips; //kan meerdere soundeffects hebben
+    public AudioClip[] clips;
 }
 
 [RequireComponent(typeof(AudioSource))]
@@ -23,25 +24,60 @@ public class SoundManager : MonoBehaviour
 {
     [SerializeField] private SoundEntry[] soundList;
 
+    [Header("Mixer Groups")]
+    [SerializeField] private AudioMixerGroup enemiesMixerGroup;
+    [SerializeField] private AudioMixerGroup gunsMixerGroup;
+    // Add more groups here as needed
+
     private static SoundManager instance;
-    private AudioSource audioSource;
+    private AudioSource defaultAudioSource;
+    private AudioSource enemiesAudioSource;
+    private AudioSource gunsAudioSource;
+
     private Dictionary<SoundType, AudioClip[]> soundDictionary;
+
+    // Define which SoundTypes belong to which group
+    private static readonly HashSet<SoundType> enemySounds = new()
+    {
+        SoundType.ENEMYWALK,
+        SoundType.ENEMYDEATH,
+        SoundType.PEEPER,
+        SoundType.ENEMYAGRO,
+    };
+
+    private static readonly HashSet<SoundType> gunSounds = new()
+    {
+        SoundType.TOKAREV,
+    };
 
     private void Awake()
     {
         instance = this;
 
         soundDictionary = new Dictionary<SoundType, AudioClip[]>();
-
         foreach (var entry in soundList)
-        {
             soundDictionary[entry.soundType] = entry.clips;
-        }
+
+        // The original AudioSource becomes the default (Master)
+        defaultAudioSource = GetComponent<AudioSource>();
+
+        // Create extra AudioSources for each mixer group
+        enemiesAudioSource = CreateAudioSource(enemiesMixerGroup);
+        gunsAudioSource = CreateAudioSource(gunsMixerGroup);
     }
 
-    private void Start()
+    private AudioSource CreateAudioSource(AudioMixerGroup mixerGroup)
     {
-        audioSource = GetComponent<AudioSource>();
+        AudioSource source = gameObject.AddComponent<AudioSource>();
+        source.outputAudioMixerGroup = mixerGroup;
+        return source;
+    }
+
+    private AudioSource GetAudioSource(SoundType sound)
+    {
+        if (enemySounds.Contains(sound)) return enemiesAudioSource;
+        if (gunSounds.Contains(sound)) return gunsAudioSource;
+        return defaultAudioSource;
     }
 
     public static void PlaySound(SoundType sound, float volume = 1)
@@ -55,7 +91,7 @@ public class SoundManager : MonoBehaviour
         if (instance.soundDictionary.TryGetValue(sound, out AudioClip[] clips))
         {
             AudioClip randomClip = clips[Random.Range(0, clips.Length)];
-            instance.audioSource.PlayOneShot(randomClip, volume);
+            instance.GetAudioSource(sound).PlayOneShot(randomClip, volume);
         }
         else
         {
